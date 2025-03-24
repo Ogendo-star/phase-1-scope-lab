adb7d19717d78c64c1877ce1d9c5f0ee4a620c1d
Masela Ogendo <puroselah@gmail.com>
2025-03-24 03:16:50 +0300

    feat:Performed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+};
+
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer(name) {
+    bestCustomer = name;
+};
+
+const leastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer() {
+    throw new Error('Assignment to constant variable.'); 
+};
